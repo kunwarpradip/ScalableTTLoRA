01bb8bca6a7985f618c695f9bc3221721c4f4e61
user <nitrkl112@gmail.com>
2025-01-29 22:47:04 -0600

    Code for creating multitasking dataset and train the moe with 4 experts

---
+++ eight_train_ttmoe_multitask.py
@@ -0,0 +1,209 @@
+import pytorch_lightning as pl
+import pandas as pd
+import torch
+import tensorly as tl
+import time
+import os
+import math
+import sys
+
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+from torch.utils.data import DataLoader
+from argparse import ArgumentParser
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping
+from functools import partial
+from datasets import load_dataset, Dataset
+
+from torch import nn
+from model import CustomLightningModule
+from utils import get_tokenizer, load_local_dataset, load_mixed_datasets
+from one_ttlora_wrapper import TTLoRALinearWrapper
+from eight_ttmoe_multitask_wrapper import MoEsparseRouting
+from create_experts import parse_expert_files, print_experts_details, save_adapter_weights
+import matplotlib.pyplot as plt
+
+tl.set_backend('pytorch')
+# Redirect stdout and stderr to a file
+sys.stdout = open('output.log', 'w')
+sys.stderr = open('output.log', 'w')
+
+datasets = ["cola", "qnli", "sst2", "mrpc"]  # Define datasets to mix
+model_name = "roberta-base"
+model_path = "./roberta-base/roberta-base-model"
+tokenizer_path = "./roberta-base/roberta-base-tokenizer"
+# torch.autograd.set_detect_anomaly(True)
+
+def apply_hooks(model):
+    def forward_hook(module, input, output):
+        if not hasattr(forward_hook, "called"):
+            forward_hook.called = True
+            print("*"*10,"Inside forward hook; Looking into grad and grad_fn whose grad_fn is not None","*"*10)
+        if isinstance(input, tuple):
+            for i, inp in enumerate(input):
+                if isinstance(inp, torch.Tensor):
+                    if inp.grad_fn is not None:
+                        print(f"{"Module Name", module.__class__.__name__}")
+                        print(f"Shape: {inp.shape}, grad_fn: {inp.grad_fn}")
+        else:
+            if input.grad_fn is not None:
+                print(f"{"Module Name", module.__class__.__name__}")
+                print(f"Shape: {input.shape}, grad_fn: {input.grad_fn}")
+        if isinstance(output, torch.Tensor):
+            if output.grad_fn is not None:
+                print(f"{"Module Name", module.__class__.__name__}")
+                print(f"Shape: {output.shape}, grad_fn: {output.grad_fn}")
+        elif isinstance(output, tuple):
+            for i, out in enumerate(output):
+                if isinstance(out, torch.Tensor):
+                    if out.grad_fn is not None:
+                        print(f"{"Module Name", module.__class__.__name__}")
+                        print(f"Shape: {out.shape}, grad_fn: {out.grad_fn}")
+
+    for name, module in model.named_modules():
+        module.register_forward_hook(forward_hook)
+
+    def backward_hook(module, grad_input, grad_output):
+        if not hasattr(backward_hook, "called"):
+            backward_hook.called = True
+            print("*"*10,"Inside Backward hook; Looking into input and output grad at layers with requires_grad as True","*"*10)
+        if any(param.requires_grad for param in module.parameters()):
+            for i, grad in enumerate(grad_input):
+                print(f"Module: {module.__class__.__name__}")
+                print(f"Grad input: {grad}")
+            for i, grad in enumerate(grad_output):
+                print(f"Module: {module.__class__.__name__}")
+                print(f"Grad Output: {grad}")
+    
+    for name, module in model.named_modules():
+        module.register_full_backward_hook(backward_hook)
+
+def train_moe_without_ray(config):
+    
+    if not torch.cuda.is_available():
+        print("Please switch to a GPU machine before running this code.")
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    #load experts
+    saved_adapters_path= config["saved_adapter_path"]
+    experts = parse_expert_files(saved_adapters_path) #dictionary of experts
+
+    '''Load the model and and define the labels'''
+    model = AutoModelForSequenceClassification.from_pretrained(model_path, num_labels=2)
+    model.config.pad_token_id = model.config.eos_token_id
+
+    if model_name == "roberta-base":
+        model.roberta.encoder = MoEsparseRouting(model.roberta.encoder, 
+                                                experts, 
+                                                config["common_alpha"],
+                                                config["m_factors"], 
+                                                config["n_factors"],
+                                                train_router_only=True,
+                                                device=device)
+        
+    # apply_hooks(model)
+
+    for name, param in model.named_parameters():
+        if "router" in name:
+            param.requires_grad = True
+        else:
+            param.requires_grad = False
+    
+    '''Dataset loading and check if loaded correctly'''
+    train_dataset_dict, val_dataset_dict = load_mixed_datasets(datasets, tokenizer_path)
+    
+    train_dataset = Dataset.from_dict(train_dataset_dict)
+    val_dataset = Dataset.from_dict(val_dataset_dict)
+
+    # print("train dataset feature types", type(train_dataset["input_ids"]), type(train_dataset["attention_mask"]), type(train_dataset["label"]))
+    train_dataset.set_format("torch", columns=["input_ids", "attention_mask", "label"])
+    val_dataset.set_format("torch", columns=["input_ids", "attention_mask", "label"])
+
+    print("Train dataset shapes: ", train_dataset['input_ids'].shape, train_dataset["input_ids"].dtype, train_dataset['attention_mask'].shape, train_dataset['label'].shape)
+    print("Validation dataset shapes: ", val_dataset['input_ids'].shape, val_dataset['attention_mask'].shape, val_dataset['label'].shape)
+
+    '''Dataloader (an iterable) handles number of rows in each batch and how many gpus to use'''
+    train_loader = DataLoader(
+        dataset=train_dataset,
+        batch_size=128, #max of roberta base is 512 tokens
+        shuffle=True,   #data shuffles at the beginning of each epoch
+        num_workers=4   #separate subprocesses to load data in parallel
+    )
+
+    val_loader = DataLoader(
+        dataset=val_dataset,
+        batch_size=128,
+        num_workers=4
+        #no need to shuffle the validation data as to get the consistent evaluations
+    )
+
+    '''For trainig and evaluation'''
+    lightning_model = CustomLightningModule(model, 
+                                            datasets, 
+                                            config["learning_rate"])
+    
+    early_stopping_callback = EarlyStopping(
+        monitor='val_loss',
+        patience=30,
+        verbose=True,
+        mode='min'
+        )
+
+    '''Callback provided by PyTorch Lightning that allows to save model checkpoints during training'''
+    model_checkpoint_callback=ModelCheckpoint(save_top_k=1, mode="max", monitor="val_acc")  
+
+    trainer = pl.Trainer(
+        max_epochs=100,
+        callbacks=[early_stopping_callback, model_checkpoint_callback],
+        accelerator="gpu",
+        precision="16-mixed",
+        devices=args.gpus,
+        # enable_progress_bar=True,
+        # enable_model_summary=False, 
+        log_every_n_steps=10,
+    )
+    
+    start = time.time()
+    trainer.fit(model=lightning_model,
+                train_dataloaders=train_loader,
+                val_dataloaders=val_loader,
+                #checkpoint_path is used to load the model from the checkpoint to resume the training
+                # ckpt_path="./lightning_logs/version_2/checkpoints/epoch=0-step=819.ckpt"
+                )
+    end = time.time()
+    elapsed = end - start
+    print(f"Time elapsed {elapsed/60:.2f} min")
+
+    '''Model Testing in test and validation datasets'''
+    train_acc = trainer.test(lightning_model, dataloaders=train_loader, ckpt_path="best", verbose=False)
+    val_acc=trainer.test(lightning_model, dataloaders=val_loader, ckpt_path="best", verbose=False)
+    print("-"*50, "Training Accuracy: ", train_acc, "Validation Accuracy: ", val_acc)
+
+    def count_parameters(model):
+        return sum(p.numel() for p in model.parameters() if p.requires_grad)
+    train_params = count_parameters(model)
+
+    return {"Taining_Accuracy": train_acc[0]['accuracy'], "Validation_Accuray": val_acc[0]['accuracy'], "Trainable_parameters_count": train_params}
+
+def main():
+    config = {
+        "saved_adapter_path": "./saved_adapters",
+        "common_alpha" : 8,
+        "learning_rate":  1e-2,
+        "m_factors": [12,8,8],
+        "n_factors": [2,2,2,8,12]
+    }
+    
+    analysis =  train_moe_without_ray(config)
+    df = pd.DataFrame(list(analysis.items()), columns=['metric', 'value'])
+    print(df)
+    filename = f"MoE_multitask_{model_name}_test.csv"
+    df.to_csv(filename, index=False)
+    # train_moe_without_ray(config)
+
+if __name__ == "__main__":
+    
+    parser = ArgumentParser()
+    parser.add_argument("--gpus", type=int, default=1)
+    args = parser.parse_args()
+    main()
--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 import torch.nn.functional as F 
 import math
 
-custom_cache_dir = "../cache_hf/"
+custom_cache_dir = "~/.cache/huggingface/"
 
 os.environ['HF_DATASETS_CACHE'] = custom_cache_dir
 
@@ -24,7 +24,7 @@ def get_ttlora_rank(r, ttlora_shape):
     return ttlora_rank
 
 def load_local_dataset(data_name):
-    path = "../../MoE_OthersWorks/llmCourse_MoE/required_codes/data"
+    path = "./data/"
     data_path = os.path.join(path, data_name)
     dataset = load_dataset(data_path)
     return dataset
@@ -71,7 +71,7 @@ def get_tokenizer(path, data_name, dataset):
     # None batch size = process entire dataset as single batch
     tokenized = dataset.map(tokenize_text, batched=True, batch_size=None) 
 
-    # # Show some examples of the tokenized data of the train set
+    # Show some examples of the tokenized data of the train set
     # print("---------------------------------------------------------")
     # print("\nThis is how it looks like after tokenization is done \n",tokenized)
     # for i in range(1):
@@ -84,15 +84,117 @@ def get_tokenizer(path, data_name, dataset):
     #     print("Decoded Tokenized: ", tokenizer.decode(tokenized['train'][i]['input_ids']),"\n")
     #     print("Attention mask: ", (tokenized['train'][i]['attention_mask']),"\n")
 
-    ### Which columns to keep?
+    ### change the format into tensors of the specific columns
     tokenized.set_format("torch", columns=["input_ids", "attention_mask", "label"])
     # # Show an example after converting to tensors
     # print("---------------------------------------------------------")
     # print("\nThis is how it looks like after converting to tensors \n")
-    # for i in range(1):
-    #     print(f"Example {i+1}:")
-    #     print("Input IDs: ", tokenized['train'][i]['input_ids'],"\n")
-    #     print("Attention Mask: ", tokenized['train'][i]['attention_mask'],"\n")
-    #     print("Label: ", tokenized['train'][i]['label'],"\n")
+    # print("Input IDs: ", tokenized['train']['input_ids'].shape,"\n")
+    # print("Attention Mask: ", tokenized['train']['attention_mask'].shape,"\n")
+    # print("Label: ", tokenized['train']['label'].shape,"\n")
 
     return tokenized
+
+def get_mix_tokenizer(path, data_name, dataset):
+
+    tokenizer = AutoTokenizer.from_pretrained(path)
+    
+    def tokenize_text(batch):
+        if data_name == "sst2":
+            return tokenizer(batch["sentence"], add_special_tokens=True, truncation=True, padding='max_length', max_length=512)
+        if data_name == "mrpc":
+            return tokenizer(batch["sentence1"], batch['sentence2'], add_special_tokens=True, truncation=True, padding='max_length', max_length=512)
+        if data_name == "cola":
+            return tokenizer(batch["sentence"], add_special_tokens=True, truncation=True, padding='max_length', max_length=512)
+        if data_name == "qnli":
+            return tokenizer(batch["question"], batch['sentence'], add_special_tokens=True, truncation=True, padding='max_length', max_length=512)
+        if data_name == "rte":
+            return tokenizer(batch["sentence1"], batch['sentence2'], add_special_tokens=True, truncation=True, padding='max_length', max_length=512)
+        if data_name == "mnli":
+            return tokenizer(batch["premise"], batch['hypothesis'], add_special_tokens=True, truncation=True, padding='max_length', max_length=512)
+        if data_name == "qqp":
+            return tokenizer(batch["question1"], batch['question2'], add_special_tokens=True, truncation=True, padding='max_length', max_length=512)
+        if data_name == "stsb":
+            return tokenizer(batch["sentence1"], batch['sentence2'], add_special_tokens=True, truncation=True, padding='max_length', max_length=512)
+        if data_name == "wsc":
+            return tokenizer(batch["text"], batch['span1_text'], batch['span2_text'], add_special_tokens=True, truncation=True, padding='max_length', max_length=512)
+        if data_name == "winogrande":
+            return tokenizer(batch["sentence"], batch['option1'], batch['option2'], batch['answer'], add_special_tokens=True, truncation=True, padding='max_length', max_length=512)
+        if data_name == "ax":
+            return tokenizer(batch["premise"], batch['hypothesis'], add_special_tokens=True, truncation=True, padding='max_length', max_length=512)
+        if data_name == "multirc":
+            return tokenizer(batch["paragraph"], batch['question'], batch['answer'], add_special_tokens=True, truncation=True, padding='max_length', max_length=512)
+        if data_name == "boolq":
+            return tokenizer(batch["question"], batch['passage'], add_special_tokens=True, truncation=True, padding='max_length', max_length=512)
+        if data_name == "hellaswag":
+            return tokenizer(batch["ind"], batch["activity_label"], batch['ctx_a'],batch['ctx_b'],batch['ctx'],batch['endings'],batch['source_id'],batch['split'],batch['split_type'], add_special_tokens=True, truncation=True, padding='max_length', max_length=512)
+
+    tokenized = dataset.map(tokenize_text, batched=True, batch_size=None) 
+    tokenized.set_format("torch", columns=["input_ids", "attention_mask", "label"])
+    return tokenized
+
+def load_mixed_datasets(dataset_names, tokenizer_path):
+    '''Dataset loading and check if loaded correctly'''
+    mixed_train_dataset_dict = {
+        
+        "input_ids": torch.empty(0,dtype=torch.int64),
+        "attention_mask": torch.empty(0, dtype=torch.int64),
+        "label": torch.empty(0, dtype=torch.int64),
+    }
+    mixed_validation_dataset_dict = {
+        "input_ids": torch.empty(0, dtype=torch.int64),
+        "attention_mask": torch.empty(0, dtype=torch.int64),
+        "label": torch.empty(0, dtype=torch.int64),
+    }
+    for dataset_name in dataset_names:
+        print("Loading dataset: ", dataset_name)
+        dataset = load_dataset("glue",dataset_name)
+        tokenized = get_mix_tokenizer(tokenizer_path, dataset_name , dataset)
+        train_tokenized_dataset = tokenized["train"]
+        train_tokenized_dataset = train_tokenized_dataset.remove_columns(
+            [col for col in train_tokenized_dataset.column_names if col not in ["input_ids", "attention_mask", "label"]]
+        )
+        print("Train tokenized dataset: ", train_tokenized_dataset['input_ids'].shape, train_tokenized_dataset['attention_mask'].shape, train_tokenized_dataset['label'].shape)
+        validation_tokenized_dataset = tokenized["validation"]
+        validation_tokenized_dataset = validation_tokenized_dataset.remove_columns(
+            [col for col in train_tokenized_dataset.column_names if col not in ["input_ids", "attention_mask", "label"]]
+        )
+        print("Validation tokenized dataset: ", validation_tokenized_dataset['input_ids'].shape, validation_tokenized_dataset['attention_mask'].shape, validation_tokenized_dataset['label'].shape)
+
+        #########################################For Train###################################################
+        mixed_train_dataset_dict["input_ids"] = torch.cat((mixed_train_dataset_dict["input_ids"], 
+                                                           train_tokenized_dataset["input_ids"]), dim=0)
+        mixed_train_dataset_dict["attention_mask"] = torch.cat((mixed_train_dataset_dict["attention_mask"], 
+                                                                train_tokenized_dataset["attention_mask"]), dim=0)
+        mixed_train_dataset_dict["label"] = torch.cat((mixed_train_dataset_dict["label"], 
+                                                       train_tokenized_dataset["label"]), dim=0)
+        #########################################For Validation###################################################
+
+        mixed_validation_dataset_dict["input_ids"] = torch.cat((mixed_validation_dataset_dict["input_ids"], 
+                                                                validation_tokenized_dataset["input_ids"]), dim=0)
+        mixed_validation_dataset_dict["attention_mask"] = torch.cat((mixed_validation_dataset_dict["attention_mask"], 
+                                                                     validation_tokenized_dataset["attention_mask"]), dim=0)
+        mixed_validation_dataset_dict["label"] = torch.cat((mixed_validation_dataset_dict["label"], 
+                                                            validation_tokenized_dataset["label"]), dim=0)
+    
+    print("mixed_train_dataset_dict: ", 
+          mixed_train_dataset_dict['input_ids'].shape, 
+          mixed_train_dataset_dict['attention_mask'].shape, 
+          mixed_train_dataset_dict['label'].shape,
+          "Data types: ", 
+          mixed_train_dataset_dict['input_ids'].dtype, 
+          mixed_train_dataset_dict['attention_mask'].dtype, 
+          mixed_train_dataset_dict['label'].dtype
+          )
+    print("mixed_validation_dataset_dict: ", 
+          mixed_validation_dataset_dict['input_ids'].shape, 
+          mixed_validation_dataset_dict['attention_mask'].shape, 
+          mixed_validation_dataset_dict['label'].shape,
+          "Data types: ", 
+          mixed_validation_dataset_dict['input_ids'].dtype, 
+          mixed_validation_dataset_dict['attention_mask'].dtype, 
+          mixed_validation_dataset_dict['label'].dtype
+          )
+    print(mixed_train_dataset_dict['label'].shape, mixed_train_dataset_dict['label'])
+    return mixed_train_dataset_dict, mixed_validation_dataset_dict
+
